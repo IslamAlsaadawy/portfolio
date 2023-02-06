e96ba044e8d44dd044ead56aeca4ceb63893a087
IslamAlsaadawy <islamalsaadawy12@gmail.com>
2023-02-06 21:01:56 +0200

    initial

---
+++ src/sections/floating-nav/FloatingNav.jsx
@@ -0,0 +1,8 @@
+import './floatingnav.css'
+const FloatingNav = () => {
+  return (
+    <section id="floating__nav"></section>
+  )
+}
+
+export default FloatingNav
---
+++ src/sections/header/Header.jsx
@@ -0,0 +1,8 @@
+import './header.css'
+const Header = () => {
+  return (
+    <header id="header"><h2>header</h2></header>
+  )
+}
+
+export default Header
