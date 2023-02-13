10570d344cbd91dcd2c7b2d492bb07c82b81db48
IslamAlsaadawy <islamalsaadawy12@gmail.com>
2023-02-14 00:39:48 +0200

    Finished header section

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,7 +1,28 @@
 import './header.css'
+import data from './data'
+import HeaderImage from '../../assets/pexels-pixabay-36469.jpg'
 const Header = () => {
   return (
-    <header id="header"><h2>header</h2></header>
+    <header id="header">
+      <div className="container header__container">
+        <div className="header__profile">
+          <img src={HeaderImage} alt="Header Portrait" />
+        </div>
+        <h3>Islam Alsaadawy</h3>
+        <p>You are a step away from having your website created by me!</p>
+        <div className="header__cta">
+          <a href="#contact" className='btn primary'>Let's talk</a>
+          <a href="#portfolio" className='btn light'>My work</a>
+
+        </div>
+        <div className="header__socials">
+          {
+            data.map(item => <a key= {item.id} href={item.link} target="_blank" rel="noopener noreferrer">{item.icon}</a> )
+          }
+        </div>
+      </div>
+
+    </header>
   )
 }
 
