b7b5bee004ac86e5e057b5ec5315ba89aba9608f
IslamAlsaadawy <islamalsaadawy12@gmail.com>
2023-02-18 20:47:43 +0200

    feat: added card component

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,7 +1,41 @@
 import './About.css'
+import AboutImage from '../../assets/pexels-pixabay-36469.jpg'
+import CV from '../../assets/Islam alsaadawy CV.pdf'
+import {BsDownload} from 'react-icons/bs'
+import data from './data'
+import Card from '../../components/Card'
 const About = () => {
   return (
-    <section id="about"><h2>About</h2></section>
+    <section id="about">
+      <div className="container about__container">
+        <div className="about__left">
+          <div className="about__portrait">
+            <img src={AboutImage} alt="" />
+          </div>
+        </div>
+        <div className="about__right">
+          <h2>About me</h2>
+          <div className="about__cards">
+              {
+                data.map(item =>  (
+                  <Card key={item.id} className="about__card">
+                      <span className="about__card-icon">{item.icon}</span>
+                      <h5>{item.title}</h5>
+                      <small>{item.desc}</small>
+                  </Card>
+                ))
+              }
+          </div>
+          <p>
+            Hi my name is Islam Alsaadawy i am a full stack developer that strives
+      
+          </p>  
+          <a href={CV} download className='btn primary'>Download CV <BsDownload/></a>
+        </div>
+
+        </div>
+
+    </section>
   )
 }
 
---
+++ src/sections/about/data.js
@@ -0,0 +1,13 @@
+import {FaAward} from 'react-icons/fa'
+import {TbBooks} from 'react-icons/tb'
+import {BiHappyHeartEyes} from 'react-icons/bi'
+
+
+
+const data = [
+    {id:1,icon:<FaAward/> ,title:"Experience", desc:"2+ Years working"},
+    {id:1,icon:<TbBooks/>, title:"Projects", desc:"20+ Completed"},
+    {id:1,icon:<BiHappyHeartEyes/> ,title:"Clients", desc:"30+ Happy clients"},
+
+]
+export default data;
